79e75dd34a9e6dade0582882b8aa11d3032818b6
Long Hoang <longhoang0304@users.noreply.github.com>
2026-03-23 21:57:56 +0000

    chore: upgrade vite to v8 and related deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://localhost:8443',
         changeOrigin: true,
         secure: false, // Mirth often uses self-signed certs
-      }
-    }
-  }
+      },
+    },
+  },
 })
